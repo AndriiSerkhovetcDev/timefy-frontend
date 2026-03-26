77bdbe70275e6014e4fb8c61ec270e8cbee2c6d0
AndriiSerkhovetcDev <andrii.serkhovetc.dev@gmail.com>
2026-03-26 18:12:10 +0200

    added  form for email or login recovery and check email

--- src/features/auth/model/shemas.ts
+++ src/features/auth/model/shemas.ts
@@ -44,6 +44,37 @@ export const registerSchema = z
     path: ["confirm_password"],
   });
 
+export const forgotPassEmailStepSchema = z.object({
+  email_login: z
+    .string()
+    .min(1, "Поле не може бути порожнім")
+    .refine(
+      (val) => {
+        const isEmail = val.includes("@");
+        if (isEmail) return z.email().safeParse(val).success;
+        return /^[a-zA-Z0-9_]+$/.test(val);
+      },
+      { message: "Введіть коректний email або логін" },
+    )
+    .transform((val) => val.trim().toLowerCase()),
+});
+
+export const forgotPassResetSchema = z.object({
+  password: z
+    .string()
+    .min(8, "Пароль має бути не менше 8 символів")
+    .regex(/[A-Z]/, "Має містити велику літеру")
+    .regex(/[0-9]/, "Має містити цифру")
+    .regex(/[!@#$%^&*]/, "Має містити спеціальний символ")
+    .transform((val) => val.trim()),
+  confirm_password: z
+    .string()
+    .min(1, "Підтвердіть пароль")
+    .transform((val) => val.trim()),
+});
+
 //types
 export type LoginFormValues = z.infer<typeof loginSchema>;
 export type RegisterFormValues = z.infer<typeof registerSchema>;
+export type ForgotPassEmailStepValue = z.infer<typeof forgotPassEmailStepSchema>;
+export type ForgotPassResetValues = z.infer<typeof forgotPassResetSchema>;
