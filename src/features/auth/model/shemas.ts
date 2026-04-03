b95f372afd391a64ac450d8c2264117f6444f55f
AndriiSerkhovetcDev <andrii.serkhovetc.dev@gmail.com>
2026-04-03 15:09:05 +0300

    fix schema validate

--- src/features/auth/model/shemas.ts
+++ src/features/auth/model/shemas.ts
@@ -56,19 +56,24 @@ export const forgotPassEmailStepSchema = z.object({
     .transform((val) => val.trim().toLowerCase()),
 });
 
-export const forgotPassResetSchema = z.object({
-  password: z
-    .string()
-    .min(8, "Пароль має бути не менше 8 символів")
-    .regex(/[A-Z]/, "Має містити велику літеру")
-    .regex(/[0-9]/, "Має містити цифру")
-    .regex(/[!@#$%^&*]/, "Має містити спеціальний символ")
-    .transform((val) => val.trim()),
-  confirm_password: z
-    .string()
-    .min(1, "Підтвердіть пароль")
-    .transform((val) => val.trim()),
-});
+export const forgotPassResetSchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, "Пароль має бути не менше 8 символів")
+      .regex(/[A-Z]/, "Має містити велику літеру")
+      .regex(/[0-9]/, "Має містити цифру")
+      .regex(/[!@#$%^&*]/, "Має містити спеціальний символ")
+      .transform((val) => val.trim()),
+    confirm_password: z
+      .string()
+      .min(1, "Підтвердіть пароль")
+      .transform((val) => val.trim()),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "Паролі не співпадають",
+    path: ["confirm_password"],
+  });
 
 //types
 export type LoginFormValues = z.infer<typeof loginSchema>;
